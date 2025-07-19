9b143b206b4e608f41a264bed9ecf37d18b4b7d8
Tan Kang Soon <tankangsoon@gmail.com>
2025-07-19 17:52:46 +0800

    This somewhat works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,101 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private LerpProcess<float> _floatProcess;
-    private ProcessTicker _processTicker = new ProcessTicker();
+    private LerpProcess<float> floatProcess;
 
     private void Start()
     {
-        _floatProcess = new LerpProcess<float>(0, 10, 10, Mathf.Lerp, (t) => t);
-        _floatProcess.Start();
-        _processTicker.Add(ref _floatProcess);
+        floatProcess = new LerpProcess<float>(0, 10, 10, Mathf.Lerp, (t) => t);
+        floatProcess.Start();
+        LerpProcess<float>.Add(ref floatProcess);
+
+        var vector3Process = new LerpProcess<Vector3>(Vector3.zero, Vector3.one, 10, Vector3.Lerp, (t) => t);
+        vector3Process.Start();
+        LerpProcess<Vector3>.Add(ref vector3Process);
     }
 
     private void Update()
     {
-        _processTicker.Update();
-        _floatProcess.Update();
+        LerpProcess<float>.UpdateAll();
+        LerpProcess<Vector3>.UpdateAll();
     }
-}
-
-public enum ProcessType
-{
-    // TODO: Add more types here
-    Simple,
-    LerpFloat,
-}
-
-public sealed class ProcessTicker
-{
-    // Separate arrays for each process type
-    private SimpleProcess[] _simpleProcesses = new SimpleProcess[64];
-    private LerpProcess<float>[] _floatProcesses = new LerpProcess<float>[64];
-    // Add arrays for other process types
-
-    private int _simpleCount;
-    private int _floatCount;
-    // Add counts for other types
-
-    public void Add(ref SimpleProcess process)
-    {
-        if (_simpleCount >= _simpleProcesses.Length)
-            Array.Resize(ref _simpleProcesses, _simpleProcesses.Length * 2);
-
-        _simpleProcesses[_simpleCount++] = process;
-    }
-
-    public void Add(ref LerpProcess<float> process)
-    {
-        if (_floatCount >= _floatProcesses.Length)
-            Array.Resize(ref _floatProcesses, _floatProcesses.Length * 2);
-
-        _floatProcesses[_floatCount++] = process;
-    }
-
-    public void Update()
-    {
-        UpdateSimpleProcesses();
-        UpdateFloatProcesses();
-        // Update other process types
-    }
-
-    private void UpdateSimpleProcesses()
-    {
-        int index = 0;
-        while (index < _simpleCount)
-        {
-            ref SimpleProcess proc = ref _simpleProcesses[index];
-            proc.Update();
-
-            if (proc.IsDone)
-            {
-                // Swap with last element and reduce count
-                _simpleProcesses[index] = _simpleProcesses[_simpleCount - 1];
-                _simpleCount--;
-            }
-            else index++;
-        }
-    }
-
-    private void UpdateFloatProcesses()
-    {
-        int index = 0;
-        while (index < _floatCount)
-        {
-            ref LerpProcess<float> proc = ref _floatProcesses[index];
-            proc.Update();
-
-            if (proc.IsDone)
-            {
-                // Swap with last element and reduce count
-                _floatProcesses[index] = _floatProcesses[_floatCount - 1];
-                _floatCount--;
-            }
-            else index++;
-        }
-    }
-
-    // Add similar update methods for other process types
 }
 
 public struct SimpleProcess 
@@ -189,9 +112,39 @@ public struct LerpProcess<T>
         {
             _value = _lerp(_start, _end, _easing(_elapsedTime/_duration));
             _elapsedTime += Time.deltaTime;
+            Debug.Log(_value);
             return;
         }
+        _value = _end;
         _onComplete?.Invoke();
         _isDone = true;
+    }
+
+    private static LerpProcess<T>[] _processes = new LerpProcess<T>[64];
+    private static int _processCount;
+
+    public static void Add(ref LerpProcess<T> process)
+    {
+        if (_processCount >= _processes.Length)
+            Array.Resize(ref _processes, _processes.Length * 2);
+        _processes[_processCount++] = process;
+    }
+
+    public static void UpdateAll()
+    {
+        int index = 0;
+        while (index < _processCount)
+        {
+            ref LerpProcess<T> proc = ref _processes[index];
+            proc.Update();
+
+            if (proc.IsDone)
+            {
+                // Swap with last element and reduce count
+                _processes[index] = _processes[_processCount - 1];
+                _processCount--;
+            }
+            else index++;
+        }
     }
 }
