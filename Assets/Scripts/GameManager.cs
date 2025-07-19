05b081e7d9a633c459b3851c1ce05ab3d121f238
Tan Kang Soon <tankangsoon@gmail.com>
2025-07-19 18:12:22 +0800

    Working, and pretty well!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,18 @@
 using System;
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private LerpProcess<float> floatProcess;
+    private float _value;
+    private Vector3 _test;
 
     private void Start()
     {
-        floatProcess = new LerpProcess<float>(0, 10, 10, Mathf.Lerp, (t) => t);
+        var floatProcess = new LerpProcess<float>(x => _value = x, 0, 10, 10, Mathf.Lerp, (t) => t);
         floatProcess.Start();
         LerpProcess<float>.Add(ref floatProcess);
 
-        var vector3Process = new LerpProcess<Vector3>(Vector3.zero, Vector3.one, 10, Vector3.Lerp, (t) => t);
+        var vector3Process = new LerpProcess<Vector3>(x => _test = x, Vector3.zero, Vector3.one, 10, Vector3.Lerp, (t) => t);
         vector3Process.Start();
         LerpProcess<Vector3>.Add(ref vector3Process);
     }
@@ -21,68 +21,30 @@ public class GameManager : MonoBehaviour
     {
         LerpProcess<float>.UpdateAll();
         LerpProcess<Vector3>.UpdateAll();
-    }
-}
-
-public struct SimpleProcess 
-{
-    private float _elapsedTime;
-    private float _duration;
-    private Action _onComplete;
-    private bool _isRunning;
-    private bool _isDone;
-    public bool IsDone => _isDone;
-
-    public SimpleProcess(float duration, Action onComplete = null)
-    {
-        _elapsedTime = 0;
-        _duration = duration;
-        _onComplete = onComplete;
-        _isRunning = false;
-        _isDone = false;
-    }
-
-    public void Start()
-    {
-        _isRunning = true;
-    }
-
-    public void Stop()
-    {
-        _isRunning = false;
-    }
-
-    public void Update()
-    {
-        if (!_isRunning) return;
-        if (_isDone) return;
-        if (_elapsedTime < _duration)
-            _elapsedTime += Time.deltaTime;
-        _onComplete?.Invoke();
-        _isDone = true;
+        Debug.Log(_value);
+        Debug.Log(_test);
     }
 }
 
 public struct LerpProcess<T>
 {
-    private T _value;
     private T _start;
     private T _end;
     private float _elapsedTime;
     private float _duration;
     private Func<T, T, float, T> _lerp;
     private Func<float, float> _easing;
+    private Action<T> _setter;
     private Action _onComplete;
     private bool _isRunning;
     private bool _isDone;
 
-    public T Value => _value;
     public bool IsRunning => _isRunning;
     public bool IsDone => _isDone;
 
-    public LerpProcess(T start, T end, float duration, Func<T, T, float, T> lerp, Func<float, float> easing, Action onComplete = null)
+    public LerpProcess(Action<T> setter, T start, T end, float duration, Func<T, T, float, T> lerp, Func<float, float> easing, Action onComplete = null)
     {
-        _value = start;
+        _setter = setter;
         _start = start;
         _end = end;
         _elapsedTime = 0;
@@ -110,12 +72,12 @@ public struct LerpProcess<T>
         if (_isDone) return;
         if (_elapsedTime < _duration)
         {
-            _value = _lerp(_start, _end, _easing(_elapsedTime/_duration));
+            var value = _lerp(_start, _end, _easing(_elapsedTime/_duration));
+            _setter(value);
             _elapsedTime += Time.deltaTime;
-            Debug.Log(_value);
             return;
         }
-        _value = _end;
+        _setter(_end);
         _onComplete?.Invoke();
         _isDone = true;
     }
